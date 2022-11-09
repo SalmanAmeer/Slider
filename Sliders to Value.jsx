0947a2b4b8f72288557f5f9b7763f3e0aa5c9289
Salman Ameer <59564333+SalmanAmeer@users.noreply.github.com>
2022-11-09 21:16:02 +0500

    Add files via upload

---
+++ Sliders to Value.jsx
@@ -0,0 +1,29 @@
+﻿var win = new Window ('palette',"AE Sliders",undefined);
+
+var slidgrp = win.add('group',undefined);
+slidgrp.orientation = "row";
+
+var slider1 = slidgrp.add('slider',undefined);
+slider1.value = 50;
+slider1.minvalue = 1;
+slider1.maxvalue = 150;
+
+var slidertext = slidgrp.add('edittext',undefined);
+slidertext.text = "50";
+slidertext.size = [35,25];
+
+win.show();
+win.center();
+
+//Functions
+slider1.onChange = slider1.onChanging = function(){
+        slidertext.text = Math.floor(slider1.value);
+    };
+
+slidertext.onChange = slidertext.onChanging = function(){
+        slider1.value = Math.floor(slidertext.text);
+    };
+
+
+
+
